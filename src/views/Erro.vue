35566e9b86403d95ac37d88509ce7db0cbdcd452
PedroMakengo <pedromakengomiguel@gmail.com>
2022-11-27 10:06:05 +0100

    Trabalhando nos detalhes

--- src/views/Erro.vue
+++ src/views/Erro.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
   <div class="error">
-    <h1>Error</h1>
+    <h1>Opps parece que essa página não existe :(</h1>
   </div>
 </template>
